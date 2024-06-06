549a8062e335ea5a11978844be5744effcbe6900
Hakan ÖZER <hakanozer02@gmail.com>
2024-06-06 17:33:06 +0300

    Header

--- project/src/app/inc/header/header.component.ts
+++ project/src/app/inc/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CryptoService } from '../../services/crypto.service';
+import { IUser } from '../../../models/IUser';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +11,13 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  user: IUser | null = null 
+  constructor( private crypto: CryptoService) {
+    const user = this.crypto.getUser()
+    if (user) {
+      this.user = user
+    }
+  }
 
 }
